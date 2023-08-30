36f15ad7f82aa02ef4ce9e59d149f08127afad6d
GhostMaster69-dev <rathore6375@gmail.com>
2023-08-30 07:55:39 +0530

    tulip: Initial changes for DerpFest-AOSP
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- derp_tulip.mk
+++ derp_tulip.mk
@@ -8,14 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution-X stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common DerpFest stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from tulip device
 $(call inherit-product, device/xiaomi/tulip/device.mk)
 
 # Device Info
-PRODUCT_NAME := evolution_tulip
+PRODUCT_NAME := derp_tulip
 PRODUCT_DEVICE := tulip
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 6 Pro
@@ -23,10 +23,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR_PRODUCT_NAME := tulip
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Boot Animation
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Evolution-X things
-EVO_BUILD_TYPE := OFFICIAL
-TARGET_BUILD_APERTURE_CAMERA := true
